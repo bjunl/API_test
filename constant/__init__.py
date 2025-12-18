40665dc29b7de6bf6231802d85c56f10c08c31c6
bjun <bjun.li@outlook.com>
2025-12-18 22:35:47 +0800

    feature<constant>:Restrict Import Methods.

---
+++ constant/__init__.py
@@ -0,0 +1,10 @@
+from .constant import set_variable
+from .constant import get_variable
+from .constant import clear_variables
+from .constant import remove_variable
+__all__ = [
+    "set_variable",
+    "get_variable",
+    "clear_variables",
+    "remove_variable"
+]
